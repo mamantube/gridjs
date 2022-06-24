458e904f4a49aa5d7817978be3d3c9e5608a0f2e
mamantube <firmanhiday.id007@gmail.com>
2022-06-24 11:25:11 +0700

    add new file

---
+++ tabel.js
@@ -0,0 +1,22 @@
+function tabel () {
+    new gridjs.Grid ({
+        search : true,
+        sort : true,
+        pagination : {
+            limit : 3
+        },
+        columns : ["NAMA", "UMUR", "Alamat"],
+        data : [
+            ["Anton", "27 Tahun", "Kapuk"],
+            ["Deren", "23 Tahun", "Bintaro"],
+            ["Devin", "25 Tahun", "Pondok Indah"],
+            ["Norman", "30 Tahun", "Andara"],
+            ["Rose", "24 Tahun", "Green Lake"],
+            ["Stella", "23 Tahun", "BSD"]
+        ]
+    }
+
+    ).render(document.getElementById("tabel"));
+}
+
+tabel();
